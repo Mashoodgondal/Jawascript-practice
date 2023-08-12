d7ef4c7c7bf924f1796347c519ba9e1c7b1e8c95
Mashoodgondal <mashoodgondal6146@gmail.com>
2023-08-12 10:07:18 -0700

    first commit

---
+++ basic.js
@@ -0,0 +1,60 @@
+// let weight=12;
+// for(let i=0; i<=weight; i++){
+//     if (i==13) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// *****
+// *****
+// *****
+// *****
+
+
+// let row=4;
+// let col=5;
+// let str ='';
+// for(let i=0;i<row ; i++){
+//     for(let j=0;j<col;j++){
+//         str= str +'*';
+//     }
+//     console.log(str)
+//     str='';
+// }
+
+// table
+
+
+// let num=23;
+// for(let i=1;i<=10;i++){
+//     console.log(`${num} *${i}=${num*i}`)
+// }
+
+
+// Array
+
+
+// let age=[4,5];
+// console.log(age[1])
+
+
+// let age = [5,34,5,2,78,24,8];
+// for(let i=0;i<age.length;i++){
+//     console.log(age[i]*3)
+// }
+
+// Find maximum from Array
+// Formula: Maximum  =  Numbar.MIN_SAFE_INTEGER;
+
+// let age =[304,455,57,900,68,740];
+// let maximum=Number.MIN_SAFE_INTEGER;
+// for(i=0;i<age.length;i++){
+//     if(maximum<age[i]){
+//         maximum=age[i]
+//     }
+// }
+// console.log(maximum)
+
+
+
---
+++ high-order.js
@@ -0,0 +1,32 @@
+// MAP -- REDUCE -- FILTER 
+
+// a)  MAP
+
+// let arr= [1,2,3,4,5]
+// let newarr= arr.map((num)=> num+1)
+// console.log(newarr);
+
+// let arr= [1,3,5]
+// let newarr= arr.map((num)=> num*num)
+// console.log(newarr);
+
+
+// let arr=["apple","banana","mango"]
+// let newarr= arr.map((fruits)=> fruits.toUpperCase())
+// console.log(newarr);
+
+
+//         =====> Filter <======
+
+// let arr=[2,3,45,65,6,76,7,87,8,87,5,54,4,5,6,7,7,5,4,]
+// let odd= arr.filter((num)=>num%2!==0)
+// console.log(odd);
+
+
+
+//       ======> Reduce <======
+
+// let arr= [1,5,7,8,9,7,65,4,43,32,5,4]
+// let sum= arr.reduce((current,result)=> current+result)
+// console.log(sum);
+
